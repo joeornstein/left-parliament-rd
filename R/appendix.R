b7560d4de069073104fc2c69847010412c227ee9
joeornstein <jornstein@gmail.com>
2021-03-04 14:09:15 -0500

    add kayser-lindstadt robustness test

--- R/appendix.R
+++ R/appendix.R
@@ -3,74 +3,95 @@
 ## Last Updated: Feb 11, 2021
 
 library(tidyverse)
+library(rdrobust)
 
 ## Load Kayser & Lindstadt loss probability measure (available here: http://mark-kayser.com/data.html)
 lpr <- read_delim('data/lprdata_distrib_augmented_2015.csv', delim = ';')
 
 ## Load elections dataset
-elections <- read_csv("data/IntRateCostSocDem_dataset_v2_1.csv")
+elections <- read_csv("data/IntRateCostSocDem_dataset_v2_1.csv") %>% 
+  mutate(isocode = case_when(country_name_short == 'AUS' ~ 'AUL',
+                             TRUE ~ country_name_short),
+         plurality_party = if_else(leftPluralityPercentage > 0,
+                                   SocDemPartyName, LargestOtherPartyName),
+         # harmonize some names
+         plurality_party = if_else(plurality_party == 'Fianna Fail (Soldiers of Destiny)',
+                                   'Fianna Fail', plurality_party),
+         plurality_party = if_else(plurality_party == 'Union for a Popular Movement',
+                                 'Union for a Popular Movement | The Republicans',
+                                 plurality_party),
+         plurality_party = if_else(plurality_party == 'Flemish Christian Peoples Party',
+                                   'Flemish Christian Peoples Party | Christian Democrats & Flemish',
+                                   plurality_party))
+
+# parlgov party data
+parties <- read_csv('data/parlgov_party_2020.csv') %>% 
+  mutate(isocode = case_when(country_name_short == 'AUS' ~ 'AUL',
+                             TRUE ~ country_name_short))
 
 
 # pipeline:
 
 # 1. join loss probabilities (lpr) with subsequent election
-# 2. plot x=leftPluralityPercentage vs. y=loss probability computed from previous election
+# 2. identify the previous plurality party
+# 3. compute the probability that a left party will lose their plurality (or a non-left party will keep it)
+# 4. plot x=leftPluralityPercentage vs. y=left party loss probability computed from previous election
 
 data <- elections %>% 
-  mutate(isocode = case_when(country_name_short == 'AUS' ~ 'AUL',
-                             TRUE ~ country_name_short)) %>% 
   select(isocode, 
          elecyr = election_year, 
          elecmo = election_month,
-         leftPluralityPercentage, enpp) %>% 
+         leftPluralityPercentage, enpp,
+         plurality_party) %>% 
   left_join(lpr, by = c('isocode', 'elecyr', 'elecmo')) %>% 
   # lag loss probabilities
   arrange(isocode, elecyr, elecmo) %>% 
   group_by(isocode) %>% 
   # lag previous plurality party and loss probabilities
-  mutate(previous_plurality_party = lag(PluralityParty),
+  mutate(previous_plurality_party = lag(plurality_party),
          loss_probability = lag(lpr)) %>% 
-  # compute Shannon entropy measure ('surprise')
-  mutate(entropy = if_else(loss_probability == 0, 0,
-                           -1 * (loss_probability * log2(loss_probability) + 
-                                   (1 - loss_probability) * log2(1 - loss_probability))))
-  
+  ungroup %>% 
+  select(isocode, elecyr, elecmo, leftPluralityPercentage, enpp, 
+         previous_plurality_party, loss_probability) %>%
+  # drop election-years missing from Kayser & Lindstadt data
+  filter(!is.na(loss_probability)) %>% 
+  # merge with Parlgov family names
+  left_join(parties %>% 
+              select(isocode, previous_plurality_party = party_name_english,
+                     previous_party_family = family_name), 
+            by = c('isocode', 'previous_plurality_party')) %>% 
+  mutate(left_party_loss_probability = if_else(previous_party_family == 'Social democracy',
+                                               loss_probability, 1 - loss_probability))
 
-data %>% 
-  ggplot(aes(x = leftPluralityPercentage, y = entropy)) +
-  geom_point() + 
-  geom_smooth()
 
 
 
-# dross -----
+## Appendix figure -------------
 
-#   
-# 
-# 
-# 
-# 
-# uk_elections <- elections %>% 
-#   filter(country_name_short == 'GBR') %>% 
-#   select(country_name_short, election_year, election_month, 
-#          leftPluralityPercentage, bond.yield.t, bond.yield.tplus1)
-# 
-# uk_lpr <- lpr %>% 
-#   filter(isocode == 'GBR') %>% 
-#   select(country_name_short = isocode, 
-#          election_year = elecyr, 
-#          election_month = elecmo, 
-#          PluralityParty, lpr)
-# 
-# # join together
-# uk <- left_join(uk_elections,
-#                 uk_lpr, 
-#                 by = c('country_name_short','election_year','election_month')) %>% 
-#   group_by(country_name_short) %>% 
-#   arrange(election_year, election_month) %>% 
-#   # lag previous plurality party and loss probabilities
-#   mutate(previous_plurality_party = lag(PluralityParty),
-#          loss_probability = lag(lpr),
-#          bond_yield_change = bond.yield.tplus1 - bond.yield.t,
-#          left_party_elected = as.numeric(leftPluralityPercentage > 0)) %>% 
-#   select(-PluralityParty, -lpr)
+enpp_threshold <- 3.5
+
+data_left <- data %>% filter(enpp < enpp_threshold, leftPluralityPercentage < 0)
+data_right <- data %>% filter(enpp < enpp_threshold, leftPluralityPercentage > 0)
+
+ggplot() + 
+  geom_point(color = 'gray', data = data_left, aes(x=leftPluralityPercentage,y=left_party_loss_probability)) +
+  geom_point(color = 'gray', data = data_right, aes(x=leftPluralityPercentage,y=left_party_loss_probability)) +
+  geom_smooth(color = 'black', data = data_left, 
+              aes(x=leftPluralityPercentage, y= left_party_loss_probability), se=F) +
+  geom_smooth(color = 'black', data = data_right, 
+              aes(x=leftPluralityPercentage, y= left_party_loss_probability), se=F) +
+  xlab("Left Party Plurality") + ylab("Ex Ante Left Party Loss Probability") + 
+  ylim(data %>% filter(enpp < enppThreshold) %>% pull(left_party_loss_probability) %>% min,
+       data %>% filter(enpp < enppThreshold) %>% pull(left_party_loss_probability) %>% max) +
+  xlim(-1,1) + theme_bw() + geom_vline(xintercept = 0, linetype = "dashed")
+
+X <- data %>% 
+  filter(enpp < enpp_threshold) %>% 
+  pull(leftPluralityPercentage)
+Y <- data %>% 
+  filter(enpp < enpp_threshold) %>% 
+  pull(left_party_loss_probability)
+
+
+rdModel <- rdrobust(y = Y, x = X, c = 0)
+summary(rdModel)
